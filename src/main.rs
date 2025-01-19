ec6e7cd23330be55889dbaa6a5fd9062dcf8955e
Jens Böckmann <jensboe@outlook.de>
2025-01-19 14:52:13 +0100

    Refactor track layout initialization by replacing TRACK_LAYOUT constant with mutable track_layout variable and updating main logic to utilize it

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,11 @@ mod housekeeping;
 mod track;
 
 mod track_layout;
-use track_layout::TRACK_LAYOUT;
+use track::buffer::Buffer;
+use track::straight::Straight;
+use track::switch::Switch;
+use track::{PowerState, Track};
+
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -42,17 +46,62 @@ async fn main(spawner: Spawner) {
 
 
     info!("All tasks spawned!");
+    let track_layout: &mut [&mut dyn Track]  = &mut [
+        &mut Switch::new("A_d", "D_1a", "A_c", "A_3a", 100, 100),
+        &mut Switch::new("A_c", "A_d", "A_1a", "A_2a", 100, 100),
+        &mut Straight::new("A_1a", "A_c", "A_1b", 1_500),
+        &mut Straight::new_platform("A_1b", "A_1a", "A_a", 200),
+        &mut Switch::new("A_a", "B_1a", "A_1b", "A_b", 100, 100),
 
+        &mut Straight::new("A_2a", "A_c", "A_2b", 1_500),
+        &mut Straight::new_platform("A_2b", "A_2a", "A_b", 200),
+        &mut Switch::new("A_b", "A_a", "A_3b", "A_2b", 100, 100),
 
-    let mut previous_track = TRACK_LAYOUT.iter().find(|track| track.id() == "C_3a").unwrap();
-    let mut current_track = TRACK_LAYOUT.iter().find(|track| track.id() == "C_3b").unwrap();
-    let mut next_track = current_track.get_next_track(previous_track.id());
-    loop {
-        previous_track = current_track;
-        current_track = TRACK_LAYOUT.iter().find(|track| track.id() == next_track).unwrap();
-        current_track.debug_print();
-        Timer::after(Duration::from_millis(current_track.length().into())).await;
-        next_track = current_track.get_next_track(previous_track.id());
-        debug!("Next:    {}", next_track);
+        &mut Straight::new("A_3a", "A_d", "A_3b", 1_500),
+        &mut Straight::new_platform("A_3b", "A_3a", "A_b", 200),
+
+        &mut Straight::new("B_1a", "A_a", "C_a", 6_000),
+
+        &mut Switch::new("C_a", "B_1a", "C_2a", "C_1a", 100, 100),
+        &mut Straight::new("C_1a", "C_a", "C_1b", 1_500),
+        &mut Straight::new_platform("C_1b", "C_1a", "C_b", 200),
+        &mut Straight::new("C_2a", "C_a", "C_2b", 1_500),
+        &mut Straight::new_platform("C_2b", "C_2a", "C_b", 200),
+
+        &mut Switch::new("C_b", "C_c", "C_1b", "C_2b", 100, 100),
+        &mut Switch::new("C_c", "D_1a", "C_b", "C_3c", 100, 100),
+        &mut Buffer::new("C_3a", "C_3b", 200),
+        &mut Straight::new_platform("C_3b", "C_3a", "C_3c", 200),
+        &mut Straight::new("C_3c", "C_3b", "C_c", 200),
+
+        &mut Straight::new("D_1a", "C_c", "A_d", 6_000),
+    ];
+    for track in track_layout.iter_mut() {
+        track.set_power_state(PowerState::Off);
     }
+
+    let mut current_track_id = "C_3b";
+    let mut previous_track_id = "C_3a";
+    let mut next_track_id = "";
+    let mut wait_time = 0;
+    loop {
+        for track in track_layout.iter_mut() {
+            if track.id() == current_track_id {
+                track.set_power_state(PowerState::On);
+                next_track_id = track.next_track(previous_track_id);
+                wait_time = track.length();
+                debug!("Driving from {} to {}, will take {}ms", track.id(), next_track_id, wait_time);
+            }
+        }
+        for track in track_layout.iter_mut() {
+            if track.id() == previous_track_id {
+                track.set_power_state(PowerState::Off);
+            }
+        }
+
+        Timer::after(Duration::from_millis(wait_time.into())).await;
+        previous_track_id = current_track_id;
+        current_track_id = next_track_id;
+    }    
 }
+
--- src/track_layout.rs
+++ src/track_layout.rs
@@ -1,32 +1,35 @@
 use crate::track::{buffer::Buffer, straight::Straight, switch::Switch, Track};
 
-pub const TRACK_LAYOUT: &[&dyn Track] = &[
-    &Switch::new("A_d", "D_1a", "A_c", "A_3a", 100, 100),
-    &Switch::new("A_c", "A_d", "A_1a", "A_2a", 100, 100),
-    &Straight::new("A_1a", "A_c", "A_1b", 1_500),
-    &Straight::new_platform("A_1b", "A_1a", "A_a", 200),
-    &Switch::new("A_a", "B_1a", "A_1b", "A_b", 100, 100),
 
-    &Straight::new("A_2a", "A_c", "A_2b", 1_500),
-    &Straight::new_platform("A_2b", "A_2a", "A_b", 200),
-    &Switch::new("A_b", "A_a", "A_3b", "A_2b", 100, 100),
 
-    &Straight::new("A_3a", "A_d", "A_3b", 1_500),
-    &Straight::new_platform("A_3b", "A_3a", "A_b", 200),
 
-    &Straight::new("B_1a", "A_a", "C_a", 6_000),
+pub static track_layout: &mut [&mut dyn Track]  = &mut [
+    &mut Switch::new("A_d", "D_1a", "A_c", "A_3a", 100, 100),
+    &mut Switch::new("A_c", "A_d", "A_1a", "A_2a", 100, 100),
+    &mut Straight::new("A_1a", "A_c", "A_1b", 1_500),
+    &mut Straight::new_platform("A_1b", "A_1a", "A_a", 200),
+    &mut Switch::new("A_a", "B_1a", "A_1b", "A_b", 100, 100),
 
-    &Switch::new("C_a", "B_1a", "C_2a", "C_1a", 100, 100),
-    &Straight::new("C_1a", "C_a", "C_1b", 1_500),
-    &Straight::new_platform("C_1b", "C_1a", "C_b", 200),
-    &Straight::new("C_2a", "C_a", "C_2b", 1_500),
-    &Straight::new_platform("C_2b", "C_2a", "C_b", 200),
+    &mut Straight::new("A_2a", "A_c", "A_2b", 1_500),
+    &mut Straight::new_platform("A_2b", "A_2a", "A_b", 200),
+    &mut Switch::new("A_b", "A_a", "A_3b", "A_2b", 100, 100),
 
-    &Switch::new("C_b", "C_c", "C_1b", "C_2b", 100, 100),
-    &Switch::new("C_c", "D_1a", "C_b", "C_3c", 100, 100),
-    &Buffer::new("C_3a", "C_3b", 200),
-    &Straight::new_platform("C_3b", "C_3a", "C_3c", 200),
-    &Straight::new("C_3c", "C_3b", "C_c", 200),
+    &mut Straight::new("A_3a", "A_d", "A_3b", 1_500),
+    &mut Straight::new_platform("A_3b", "A_3a", "A_b", 200),
 
-    &Straight::new("D_1a", "C_c", "A_d", 6_000),
+    &mut Straight::new("B_1a", "A_a", "C_a", 6_000),
+
+    &mut Switch::new("C_a", "B_1a", "C_2a", "C_1a", 100, 100),
+    &mut Straight::new("C_1a", "C_a", "C_1b", 1_500),
+    &mut Straight::new_platform("C_1b", "C_1a", "C_b", 200),
+    &mut Straight::new("C_2a", "C_a", "C_2b", 1_500),
+    &mut Straight::new_platform("C_2b", "C_2a", "C_b", 200),
+
+    &mut Switch::new("C_b", "C_c", "C_1b", "C_2b", 100, 100),
+    &mut Switch::new("C_c", "D_1a", "C_b", "C_3c", 100, 100),
+    &mut Buffer::new("C_3a", "C_3b", 200),
+    &mut Straight::new_platform("C_3b", "C_3a", "C_3c", 200),
+    &mut Straight::new("C_3c", "C_3b", "C_c", 200),
+
+    &mut Straight::new("D_1a", "C_c", "A_d", 6_000),
 ];
